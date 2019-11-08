519f237c8b23a66b464cd174a3207aee0f56edca
Wang Tianxiang <2743196260@qq.com>
2019-11-08 14:55:08 +0800

    2019.11.8
    完成潜力预测表单部分编写。

--- rangduju/src/components/prediction.js
+++ rangduju/src/components/prediction.js
@@ -1,9 +1,23 @@
 import React from "react";
+import PredictionForm from "./predictionForm";
+import '../common/prediction.css';
 
 class Prediction extends React.Component {
     render() {
         return (
-            <div>aaa</div>
+            <div className="prediction-page">
+                <div className="prediction-top">
+                    <div className="prediction-form">
+                        <h1 className="title">短租潜力预测</h1>
+                        <PredictionForm />
+                    </div>
+                    <div className="prediction-map">
+                        {/* 这里以后换成 map API */}
+                        <img src={require("../images/staticmap.png")} className="img-map"  alt="map"/>
+                    </div>
+                </div>
+
+            </div>
         )
     }
 }
